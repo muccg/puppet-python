6263a9b1fb976446591ff0c67a56544aad5d7c87
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-07-17 14:26:07 +0800

    Redeclared variable

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,32 +6,34 @@ define python::config(
   $index_url = 'https://pypi.python.org/simple') {
 
   if ($home == undef) {
-    $home = "/home/${owner}"
+    $homedir = "/home/${owner}"
+  } else {
+    $homedir = $home
   }
 
-  file { "$home easy install config":
+  file { "${homedir} easy install config":
     ensure  => present,
-    path    => "${home}/.pydistutils.cfg",
+    path    => "${homedir}/.pydistutils.cfg",
     content => template('python/pydistutils.cfg.erb'),
     owner   => $username,
     group   => $group
   }
 
-  file { "${home}/.pip":
+  file { "${homedir}/.pip":
     ensure => directory,
     owner  => $username,
     group  => $group
   } ->
 
-  file { "${home}/.pip/cache":
+  file { "${homedir}/.pip/cache":
     ensure => directory,
     owner  => $username,
     group  => $group
   } ->
 
-  file { "${home} pip conf":
+  file { "${homedir} pip conf":
     ensure  => present,
-    path    => "${home}/.pip/pip.conf",
+    path    => "${homedir}/.pip/pip.conf",
     content => template('python/pip.conf.erb'),
     owner   => $username,
     group   => $group
